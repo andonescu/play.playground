9ef3f3c0f3948adcf2c2f1dff25fc85fe2222dea
Ionut Andonescu <ionut.andonescu@endava.com>
2014-03-05 16:57:03 +0200

    demo project 2.2.2

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,39 +1,17 @@
 package controllers
 
 import play.api.mvc._
-import play.api.data._
-import play.api.data.Forms._
-import models.Task
 
 object Application extends Controller {
 
-
-  val taskForm = Form(
-    "label" -> nonEmptyText
-  )
-
   def index = Action {
-    Redirect(routes.Application.tasks)
+    //    Ok(views.html.index("Your new application is ready."))
+    Ok("Hello World!")
   }
 
-  def tasks = Action {
-    Ok(views.html.index(Task.all(), taskForm))
-  }
-
-  def newTask = Action {
-    implicit request =>
-      taskForm.bindFromRequest.fold(
-        errors => BadRequest(views.html.index(Task.all(), errors)),
-        label => {
-          Task.create(label)
-          Redirect(routes.Application.tasks())
-        }
-      )
-  }
-
-  def deleteTask(id: Long) = Action {
-    Task.delete(id)
-    Redirect(routes.Application.tasks)
+  def hello(name: String) = Action {
+    //    Ok(s"Hello $name!")
+    Ok(views.html.hello(name))
   }
 
 }
--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,6 +1,6 @@
-package test
-
 import org.specs2.mutable._
+import org.specs2.runner._
+import org.junit.runner._
 
 import play.api.test._
 import play.api.test.Helpers._
@@ -10,24 +10,29 @@ import play.api.test.Helpers._
  * You can mock out a whole application including requests, plugins etc.
  * For more information, consult the wiki.
  */
+@RunWith(classOf[JUnitRunner])
 class ApplicationSpec extends Specification {
-  
+
   "Application" should {
-    
-    "send 404 on a bad request" in {
-      running(FakeApplication()) {
-        route(FakeRequest(GET, "/boum")) must beNone        
-      }
+
+    "send 404 on a bad request" in new WithApplication {
+      route(FakeRequest(GET, "/boum")) must beNone
     }
-    
-    "render the index page" in {
-      running(FakeApplication()) {
-        val home = route(FakeRequest(GET, "/")).get
-        
-        status(home) must equalTo(OK)
-        contentType(home) must beSome.which(_ == "text/html")
-        contentAsString(home) must contain ("Your new application is ready.")
-      }
+
+    "render the index page" in new WithApplication {
+      val home = route(FakeRequest(GET, "/")).get
+
+      status(home) must equalTo(OK)
+      contentType(home) must beSome.which(_ == "text/html")
+      contentAsString(home) must contain("Your new application is ready.")
+    }
+
+    "verify if hello page works with a proper parameter" in new WithApplication() {
+      val hello = route(FakeRequest(GET, "/hello?name=Ionut")).get
+
+      status(hello) must equalTo(OK)
+      contentType(hello) must beSome.which(_ == "text/html")
+      contentAsString(hello) must contain("Hello ")
     }
   }
 }
